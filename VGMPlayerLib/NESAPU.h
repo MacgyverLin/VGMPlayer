49be930cd951b177e71afa7554bedbe12429819f
Mac <macgyvercct@yahoo.com.hk>
2018-12-11 17:55:11 +0800

    support vgz, add libz, add libopenal, add nes APU, add music

---
+++ VGMPlayerLib/NESAPU.h
@@ -0,0 +1,25 @@
+#ifndef _NESAPU_H_
+#define _NESAPU_H_
+
+#include "vgmdef.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void NESAPU_Initialize(UINT8 chipID, INT32 clock, UINT32 sampleRate);
+void NESAPU_Update(UINT8 chipID, INT32** buffer, INT32 samples);
+void NESAPU_Shutdown();
+void NESAPU_RESET(UINT8 chipID);
+
+void NESAPU_WriteRegister(UINT8 chipID, INT32 address, UINT8 value);
+UINT8 NESAPU_ReadRegister(UINT8 chipID, INT32 address);
+
+void NESAPU_SetIRQHandler(UINT8 chipID, UINT32(*irqHandler)());
+
+#ifdef __cplusplus
+};
+#endif
+
+
+#endif
