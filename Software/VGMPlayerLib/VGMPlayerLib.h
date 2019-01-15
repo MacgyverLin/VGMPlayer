d7c2529c3131652df9bf741f39eb50e4c4583999
Mac <macgyvercct@yahoo.com.hk>
2019-01-15 16:50:04 +0800

    1

--- Software/VGMPlayerLib/VGMPlayerLib.h
+++ Software/VGMPlayerLib/VGMPlayerLib.h
@@ -2,11 +2,15 @@
 #define _VGMPlayerLib_h_
 
 #include "vgmdef.h"
+#include "Platform.h"
+#include "Obserable.h"
 #include "VGMData.h"
 #include "VGMFile.h"
-#include "Obserable.h"
 #include "Observer.h"
 #include "VGMDataObverser.h"
+#include "VGMPlayerLib.h"
 #include "VGMAudioPlayer.h"
+#include "VGMWaveFormViewer.h"
+#include "VGMSpectrumViewer.h"
 
 #endif
