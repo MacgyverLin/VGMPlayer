f7a6c276a99772c5f85d6c41d6f4c825e2277bde
Mac <macgyvercct@yahoo.com.hk>
2019-01-17 18:12:33 +0800

    1

--- Software/VGMPlayerLib/QSound.c
+++ Software/VGMPlayerLib/QSound.c
@@ -33,91 +33,97 @@ void QSound_Update(u8 chipID, s32** buffer, u32 length)
 
 #include "vgmdef.h"
 
-struct QChan_s
+typedef struct 
 {
-	UINT8 bKey;						// 1 if channel is playing
-	INT8 nBank;						// Bank we are currently playing a sample from
+	u8 bKey;						// 1 if channel is playing
+	s8 nBank;						// Bank we are currently playing a sample from
 
-	INT32 nPlayStart;				// Start of being played
-	INT32 nStart;					// Start of sample 16.12
-	INT32 nEnd;						// End of sample   16.12
-	INT32 nLoop;					// Loop offset from end
-	INT32 nPos;						// Current position within the bank 16.12
-	INT32 nAdvance;					// Sample size
+	s32 nPlayStart;				// Start of being played
+	s32 nStart;					// Start of sample 16.12
+	s32 nEnd;						// End of sample   16.12
+	s32 nLoop;					// Loop offset from end
+	s32 nPos;						// Current position within the bank 16.12
+	s32 nAdvance;					// Sample size
 
-	INT32 nMasterVolume;			// Master volume for the channel
-	INT32 nVolume[2];				// Left & right side volumes (panning)
+	s32 nMasterVolume;			// Master volume for the channel
+	s32 nVolume[2];				// Left & right side volumes (panning)
 
-	INT32 nPitch;					// Playback frequency
+	s32 nPitch;					// Playback frequency
 
-	INT8 nEndBuffer[8];				// Buffer to enable correct cubic interpolation
+	s8 nEndBuffer[8];				// Buffer to enable correct cubic interpolation
 
-	INT8* PlayBank;					// Pointer to current bank
-};
+	s8* PlayBank;					// Pointer to current bank
+}QSoundChannel;
 
-static const INT32 nQscClock = 4000000;
-static const INT32 nQscClockDivider = 166;
-static INT32 nQscRate = 0;
-static INT32 Mmatrix;
-static INT32 nInterpolation = 3;
-static INT32 nBurnSoundLen = VGM_SAMPLE_COUNT;
-static struct QChan_s QChan[16];
-static INT32 PanningVolumes[33];
-INT8 *CpsQSam = 0; 
-UINT32 nCpsQSamLen = 0;
+typedef struct
+{
+	s32 clock;
+	s32 clockDivider;
+	s32 sampleRate;
+	s32 Mmatrix;
+	s32 interpolation;
+	QSoundChannel channels[16];
+	s32 panningVolumes[33];
+	s8 *rom;
+	u32 romLength;
+}QSound;
 
-#define INTERPOLATE4PS_CUSTOM(fp, sN, s0, s1, s2, v) (((INT32)((sN) * Precalc[(INT32)(fp) * 4 + 0]) + (INT32)((s0) * Precalc[(INT32)(fp) * 4 + 1]) + (INT32)((s1) * Precalc[(INT32)(fp) * 4 + 2]) + (INT32)((s2) * Precalc[(INT32)(fp) * 4 + 3])) / (INT32)(v))
-INT16 Precalc[4096 * 4];
+#define MAX_QSOUND 2
+QSound qSound[MAX_QSOUND];
+
+#define INTERPOLATE4PS_CUSTOM(fp, sN, s0, s1, s2, v) (((s32)((sN) * Precalc[(s32)(fp) * 4 + 0]) + (s32)((s0) * Precalc[(s32)(fp) * 4 + 1]) + (s32)((s1) * Precalc[(s32)(fp) * 4 + 2]) + (s32)((s2) * Precalc[(s32)(fp) * 4 + 3])) / (s32)(v))
+s16 Precalc[4096 * 4];
 
 // Routine used to precalculate the table used for interpolation
-INT32 cmc_4p_Precalc()
+s32 cmc_4p_Precalc()
 {
-	INT32 a, x, x2, x3;
+	s32 a, x, x2, x3;
 
 	for (a = 0; a < 4096; a++) {
 		x = a * 4;			// x = 0..16384
 		x2 = x * x / 16384;	// pow(x, 2);
 		x3 = x2 * x / 16384;	// pow(x, 3);
 
-		Precalc[a * 4 + 0] = (INT16)(-x / 3 + x2 / 2 - x3 / 6);
-		Precalc[a * 4 + 1] = (INT16)(-x / 2 - x2 + x3 / 2 + 16384);
-		Precalc[a * 4 + 2] = (INT16)(x + x2 / 2 - x3 / 2);
-		Precalc[a * 4 + 3] = (INT16)(-x / 6 + x3 / 6);
+		Precalc[a * 4 + 0] = (s16)(-x / 3 + x2 / 2 - x3 / 6);
+		Precalc[a * 4 + 1] = (s16)(-x / 2 - x2 + x3 / 2 + 16384);
+		Precalc[a * 4 + 2] = (s16)(x + x2 / 2 - x3 / 2);
+		Precalc[a * 4 + 3] = (s16)(-x / 6 + x3 / 6);
 	}
 
 	return 0;
 }
 
-static void MapBank(struct QChan_s* pc)
+void MapBank(u8 chipID, QSoundChannel* pc)
 {
-	UINT32 nBank;
-
-	nBank = (pc->nBank & 0x7F) << 16;	// Banks are 0x10000 samples long
+	QSound* ic = &qSound[chipID];
+	u32 nBank = (pc->nBank & 0x7F) << 16;	// Banks are 0x10000 samples long
 
 	// Confirm whole bank is in range:
 	// If bank is out of range use bank 0 instead
-	if ((nBank + 0x10000) > nCpsQSamLen)
+	if ((nBank + 0x10000) > ic->romLength)
 	{
 		nBank = 0;
 	}
-	pc->PlayBank = (INT8*)CpsQSam + nBank;
+	pc->PlayBank = (s8*)ic->rom + nBank;
 }
 
-static void UpdateEndBuffer(struct QChan_s* pc)
+void UpdateEndBuffer(u8 chipID, QSoundChannel* pc)
 {
+	QSound* ic = &qSound[chipID];
+
 	if (pc->bKey) 
 	{
 		// prepare a buffer to correctly interpolate the last 4 samples
-		if (nInterpolation >= 3) 
+		if(ic->interpolation >= 3)
 		{
-			for (INT32 i = 0; i < 4; i++) 
+			for (s32 i = 0; i < 4; i++) 
 			{
 				pc->nEndBuffer[i] = pc->PlayBank[(pc->nEnd >> 12) - 4 + i];
 			}
 
 			if (pc->nLoop) 
 			{
-				for (INT32 i = 0, j = 0; i < 4; i++, j++) 
+				for (s32 i = 0, j = 0; i < 4; i++, j++) 
 				{
 					if (j >= (pc->nLoop >> 12)) 
 					{
@@ -128,7 +134,7 @@ static void UpdateEndBuffer(struct QChan_s* pc)
 			}
 			else 
 			{
-				for (INT32 i = 0; i < 4; i++) 
+				for (s32 i = 0; i < 4; i++) 
 				{
 					pc->nEndBuffer[i + 4] = pc->nEndBuffer[3];
 				}
@@ -137,23 +143,34 @@ static void UpdateEndBuffer(struct QChan_s* pc)
 	}
 }
 
-static void CalcAdvance(struct QChan_s* pc)
+void CalcAdvance(u8 chipID, QSoundChannel* pc)
 {
-	if (nQscRate) {
-		pc->nAdvance = (INT64)pc->nPitch * nQscClock / nQscClockDivider / nQscRate;
+	QSound* ic = &qSound[chipID];
+
+	if (ic->sampleRate) 
+	{
+		pc->nAdvance = (INT64)pc->nPitch * ic->clock / ic->clockDivider / ic->sampleRate;
 	}
 }
 
 s32 QSound_Initialize(u8 chipID, u32 clock, u32 sampleRate)
 {
-	nQscRate = sampleRate;
-	for (INT32 i = 0; i < 33; i++)
+	QSound* ic = &qSound[chipID];
+
+	ic->clock = 4000000;
+	ic->clockDivider = 166;
+	ic->sampleRate = sampleRate;
+	ic->Mmatrix = 0;
+	ic->interpolation = 3;
+	ic->rom = 0;
+	ic->romLength = 0;
+	for(s32 i = 0; i < 33; i++)
 	{
-		PanningVolumes[i] = (INT32)((256.0f / sqrt(32.0f)) * sqrt((double)i));
+		ic->panningVolumes[i] = (s32)((256.0f / sqrt(32.0f)) * sqrt((double)i));
 	}
 
-	//nCpsQSamLen = 0x10000 * 8;
-	//CpsQSam = (INT8*)malloc(nCpsQSamLen);
+	//romLength = 0x10000 * 8;
+	//rom = (s8*)malloc(romLength);
 	
 	cmc_4p_Precalc();
 
@@ -164,31 +181,35 @@ s32 QSound_Initialize(u8 chipID, u32 clock, u32 sampleRate)
 
 void QSound_Reset(u8 chipID)
 {
-	memset(QChan, 0, sizeof(QChan));
+	QSound* ic = &qSound[chipID];
+
+	memset(ic->channels, 0, sizeof(ic->channels));
 
 	// Point all to bank 0
-	for (INT32 i = 0; i < 16; i++)
+	for (s32 i = 0; i < 16; i++)
 	{
-		QChan[i].PlayBank = (INT8*)CpsQSam;
+		ic->channels[i].PlayBank = (s8*)ic->rom;
 	}
 }
 
 void QSound_Shutdown(u8 chipID)
 {
-	nQscRate = 0;
+	QSound* ic = &qSound[chipID];
+	ic->sampleRate = 0;
 
-	if(CpsQSam)
+	if(ic->rom)
 	{
-		free(CpsQSam);
-		CpsQSam = 0;
-		nCpsQSamLen = 0;
+		free(ic->rom);
+		ic->rom = 0;
+		ic->romLength = 0;
 	}
 }
 
 void QSound_WriteRegister(u8 chipID, u32 address, u32 data)
 {
-	struct QChan_s* pc;
-	INT32 nChanNum, r;
+	QSound* ic = &qSound[chipID];
+	QSoundChannel* pc;
+	s32 nChanNum, r;
 
 	// unknown
 	if (address >= 0x90)
@@ -200,11 +221,11 @@ void QSound_WriteRegister(u8 chipID, u32 address, u32 data)
 	//QscSyncQsnd();
 	if(address >= 0x80)
 	{									// Set panning for channel
-		INT32 nPan;
+		s32 nPan;
 
 		nChanNum = address & 15;
 
-		pc = QChan + nChanNum;			// Find channel
+		pc = ic->channels + nChanNum;			// Find channel
 		nPan = (data - 0x10) & 0x3F;	// nPan = 0x00 to 0x20 now
 		if (nPan > 0x20) 
 		{
@@ -212,13 +233,13 @@ void QSound_WriteRegister(u8 chipID, u32 address, u32 data)
 		}
 
 		// bprintf(PRINT_NORMAL, _T("QSound: ch#%i pan -> 0x%X\n"), nChanNum, nPan);
-		if (Mmatrix && nPan == 0x00) 
+		if(ic->Mmatrix && nPan == 0x00)
 		{
 			nPan = 0x10; // Fixes all sfx that are hard-panned to the right. (Mars Matrix only)
 		}
 
-		pc->nVolume[0] = PanningVolumes[0x20 - nPan];
-		pc->nVolume[1] = PanningVolumes[0x00 + nPan];
+		pc->nVolume[0] = ic->panningVolumes[0x20 - nPan];
+		pc->nVolume[1] = ic->panningVolumes[0x00 + nPan];
 		return;
 	}
 
@@ -227,17 +248,16 @@ void QSound_WriteRegister(u8 chipID, u32 address, u32 data)
 	r = address & 7;
 
 	// Pointer to channel info
-	pc = QChan + nChanNum;
-
+	pc = ic->channels + nChanNum;
 	switch (r) 
 	{
 	case 0: 
 	{										// Set bank
 		// Strange but true
-		pc = QChan + ((nChanNum + 1) & 15);
+		pc = ic->channels + ((nChanNum + 1) & 15);
 		pc->nBank = data;
-		MapBank(pc);
-		UpdateEndBuffer(pc);
+		MapBank(chipID, pc);
+		UpdateEndBuffer(chipID, pc);
 		break;
 	}
 	case 1: 
@@ -248,7 +268,7 @@ void QSound_WriteRegister(u8 chipID, u32 address, u32 data)
 	case 2: 
 	{
 		pc->nPitch = data;
-		CalcAdvance(pc);
+		CalcAdvance(chipID, pc);
 
 		if (data == 0) {					// Key off; stop playing
 			pc->bKey = 0;
@@ -265,13 +285,13 @@ void QSound_WriteRegister(u8 chipID, u32 address, u32 data)
 	case 4: 
 	{										// Set sample loop offset
 		pc->nLoop = data << 12;
-		UpdateEndBuffer(pc);
+		UpdateEndBuffer(chipID, pc);
 		break;
 	}
 	case 5: 
 	{										// Set sample end offset
 		pc->nEnd = data << 12;
-		UpdateEndBuffer(pc);
+		UpdateEndBuffer(chipID, pc);
 		break;
 	}
 	case 6: 
@@ -290,7 +310,7 @@ void QSound_WriteRegister(u8 chipID, u32 address, u32 data)
 
 				pc->nPos = 0;
 				pc->bKey = 3;
-				UpdateEndBuffer(pc);
+				UpdateEndBuffer(chipID, pc);
 			}
 		}
 		break;
@@ -306,91 +326,83 @@ void QSound_WriteRegister(u8 chipID, u32 address, u32 data)
 
 void QSound_Update(u8 chipID, s32** buffer, u32 length)
 {
+	QSound* ic = &qSound[chipID];
+
 	// Go through all channels
-	for (INT32 c = 0; c < 16; c++)
+	for (s32 c = 0; c < 16; c++)
 	{
 		// If the channel is playing, add the samples to the buffer
-		if (QChan[c].bKey)
+		if (ic->channels[c].bKey)
 		{
-			INT32 VolL = (QChan[c].nMasterVolume * QChan[c].nVolume[0]) >> 11;
-			INT32 VolR = (QChan[c].nMasterVolume * QChan[c].nVolume[1]) >> 11;
+			s32 VolL = (ic->channels[c].nMasterVolume * ic->channels[c].nVolume[0]) >> 11;
+			s32 VolR = (ic->channels[c].nMasterVolume * ic->channels[c].nVolume[1]) >> 11;
 			s32 *lBuf = buffer[0];
 			s32 *rBuf = buffer[1];
-			INT32 i = length;
+			s32 i = length;
 
 			// handle 1st sample
-			if (QChan[c].bKey & 2)
+			if (ic->channels[c].bKey & 2)
 			{
-				while (QChan[c].nPos < 0x1000 && i)
+				while (ic->channels[c].nPos < 0x1000 && i)
 				{
-					INT32 p = QChan[c].nPlayStart >> 12;
-					INT32 s = INTERPOLATE4PS_CUSTOM(QChan[c].nPos, 0, QChan[c].PlayBank[p + 0], QChan[c].PlayBank[p + 1], QChan[c].PlayBank[p + 2], 256);
+					s32 p = ic->channels[c].nPlayStart >> 12;
+					s32 s = INTERPOLATE4PS_CUSTOM(ic->channels[c].nPos, 0, ic->channels[c].PlayBank[p + 0], ic->channels[c].PlayBank[p + 1], ic->channels[c].PlayBank[p + 2], 256);
 
 					*(lBuf++) += ((s * VolL) >> 8);
 					*(rBuf++) += ((s * VolR) >> 8);
 
-					QChan[c].nPos += QChan[c].nAdvance;				// increment sample position based on pitch
+					ic->channels[c].nPos += ic->channels[c].nAdvance;				// increment sample position based on pitch
 					i--;
 				}
 				if (i > 0)
 				{
-					QChan[c].bKey &= ~2;
-					QChan[c].nPos = (QChan[c].nPos & 0x0FFF) + QChan[c].nPlayStart;
+					ic->channels[c].bKey &= ~2;
+					ic->channels[c].nPos = (ic->channels[c].nPos & 0x0FFF) + ic->channels[c].nPlayStart;
 				}
 			}
 
 			while (i > 0)
 			{
-				INT32 s, p;
+				s32 s, p;
 
 				// Check for end of sample
-				if (QChan[c].nPos >= (QChan[c].nEnd - 0x3000))
+				if (ic->channels[c].nPos >= (ic->channels[c].nEnd - 0x3000))
 				{
-					if (QChan[c].nPos < QChan[c].nEnd)
+					if (ic->channels[c].nPos < ic->channels[c].nEnd)
 					{
-						INT32 nIndex = 4 - ((QChan[c].nEnd - QChan[c].nPos) >> 12);
-						s = INTERPOLATE4PS_CUSTOM((QChan[c].nPos) & ((1 << 12) - 1),
-							QChan[c].nEndBuffer[nIndex + 0],
-							QChan[c].nEndBuffer[nIndex + 1],
-							QChan[c].nEndBuffer[nIndex + 2],
-							QChan[c].nEndBuffer[nIndex + 3],
-							256);
+						s32 nIndex = 4 - ((ic->channels[c].nEnd - ic->channels[c].nPos) >> 12);
+						s = INTERPOLATE4PS_CUSTOM((ic->channels[c].nPos) & ((1 << 12) - 1), ic->channels[c].nEndBuffer[nIndex + 0], ic->channels[c].nEndBuffer[nIndex + 1], ic->channels[c].nEndBuffer[nIndex + 2], ic->channels[c].nEndBuffer[nIndex + 3], 256);
 					}
 					else
 					{
-						if (QChan[c].nLoop)
+						if(ic->channels[c].nLoop)
 						{					// Loop sample
-							if (QChan[c].nLoop <= 0x1000)
+							if(ic->channels[c].nLoop <= 0x1000)
 							{		// Don't play, but leave bKey on
-								QChan[c].nPos = QChan[c].nEnd - 0x1000;
+								ic->channels[c].nPos = ic->channels[c].nEnd - 0x1000;
 								break;
 							}
-							QChan[c].nPos -= QChan[c].nLoop;
+							ic->channels[c].nPos -= ic->channels[c].nLoop;
 							continue;
 						}
 						else
 						{
-							QChan[c].bKey = 0;					// Stop playing
+							ic->channels[c].bKey = 0;					// Stop playing
 							break;
 						}
 					}
 				}
 				else
 				{
-					p = (QChan[c].nPos >> 12) & 0xFFFF;
-					s = INTERPOLATE4PS_CUSTOM((QChan[c].nPos) & ((1 << 12) - 1), 
-						QChan[c].PlayBank[p + 0],
-						QChan[c].PlayBank[p + 1],
-						QChan[c].PlayBank[p + 2],
-						QChan[c].PlayBank[p + 3],
-						256);
+					p = (ic->channels[c].nPos >> 12) & 0xFFFF;
+					s = INTERPOLATE4PS_CUSTOM((ic->channels[c].nPos) & ((1 << 12) - 1), ic->channels[c].PlayBank[p + 0], ic->channels[c].PlayBank[p + 1], ic->channels[c].PlayBank[p + 2], ic->channels[c].PlayBank[p + 3], 256);
 				}
 
 				// Add to the sound currently in the buffer
 				*(lBuf++) += ((s * VolL) >> 8);
 				*(rBuf++) += ((s * VolR) >> 8);
 
-				QChan[c].nPos += QChan[c].nAdvance;				// increment sample position based on pitch
+				ic->channels[c].nPos += ic->channels[c].nAdvance;				// increment sample position based on pitch
 				i--;
 			}
 		}
@@ -399,26 +411,28 @@ void QSound_Update(u8 chipID, s32** buffer, u32 length)
 
 void QSound_SetROM(u8 chipID, u32 totalROMSize, u32 startAddress, u8 *rom, u32 nLen)
 {
-	if(nCpsQSamLen != totalROMSize)
+	QSound* ic = &qSound[chipID];
+
+	if(ic->romLength != totalROMSize)
 	{
-		if (CpsQSam)
+		if(ic->rom)
 		{
 			u8* newRom = malloc(totalROMSize);
 
-			memcpy(newRom, CpsQSam, totalROMSize);
-			free(CpsQSam);
+			memcpy(newRom, ic->rom, totalROMSize);
+			free(ic->rom);
 
-			CpsQSam = newRom;
+			ic->rom = newRom;
 		}
 		else
 		{
-			CpsQSam = malloc(totalROMSize);
+			ic->rom = malloc(totalROMSize);
 		}
 
-		nCpsQSamLen = totalROMSize;
+		ic->romLength = totalROMSize;
 	}
 
-	memcpy(&CpsQSam[startAddress], rom, nLen);
+	memcpy(&ic->rom[startAddress], rom, nLen);
 }
 
 #endif
